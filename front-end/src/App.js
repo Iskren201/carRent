8879d5aa63dd778818631e9b176682ea12c1f5ef
Iskren201 <iskren201@gmail.com>
2023-11-06 11:31:48 +0200

    Fix NavigatieJs

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,17 @@
-import "./index";
+import { Navigation } from "./Components/Navigation";
+import { BrowserRouter, Routes, Route, Link, Router } from "react-router-dom";
+import "./index.css";
+import Home from "./Components/Home";
 
 function App() {
-  return <div className=""></div>;
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ front-end/src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+    return <div className="bg-white">
+        <div className="bg-gray-300 relative">
+            <div className="wrapper min-h-[444px]">
+                <h1 className="pt-28 text-center lg:text-[58px] font-extrabold sm:text-[38px]" >Find your <span className="text-green-400">Rental Car</span></h1>
+                <h4 className="text-gray-500 text-center font-bold text-xl pb-11">You choose. We deliver</h4>
+            </div>
+        </div>
+    </div>;
+};
+
+export default Home;
